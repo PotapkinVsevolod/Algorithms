3b49492d98cbf0ea17cddb3ef5be01de75942c76
hydroxyphthalimide <hydroxyphthalimide@gmail.com>
2022-10-04 19:41:42 +0700

    Test refactoring.

--- hashmap/tests/test_.py
+++ hashmap/tests/test_.py
@@ -15,12 +15,29 @@ TEST_KEYS_FOR_OVERFLOW = [
     "j",
     "k",
     "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
 ]
+
+
+class TestUsingOfDifferentTypesAsKey:
+    def test_integer(self):
+        hash_map = HashMap()
+        hash_map[1] = 2
+        assert hash_map[1] == 2
+
+    def test_none(self):
+        hash_map = HashMap()
+        hash_map[None] = 2
+        assert hash_map[None] == 2
+
+    def test_dict(self):
+        hash_map = HashMap()
+        with pytest.raises(KeyError):
+            hash_map[{"a": 1}] = 2
+
+    def test_list(self):
+        hash_map = HashMap()
+        with pytest.raises(KeyError):
+            hash_map[[]] = 2
 
 
 def test_constants():
@@ -73,21 +90,10 @@ def test_hash_function_return_the_same_hash():
     assert hash_map._get_hash("foo") == hash_map._get_hash("foo")
 
 
-def test_list_for_exist_of_12_unique_hashes():
-    hash_map = HashMap()
-    keys = TEST_KEYS_FOR_OVERFLOW
-    hashes = set()
-    for key in keys:
-        hashes.add(hash_map._get_hash(key))
-    assert len(hashes) == HashMap.INITIAL_ALLOCATED_SIZE * HashMap.MAX_LOAD_FACTOR
-
-
 def test_overflow():
     hash_map = HashMap()
-    count = 0
     for key in TEST_KEYS_FOR_OVERFLOW:
-        hash_map[key] = count
-        count += 1
+        hash_map[key] = "foo"
     assert (
         hash_map.allocated_slots
         == hash_map.INITIAL_ALLOCATED_SIZE * hash_map.EXTENSION_DEGREE
@@ -95,30 +101,8 @@ def test_overflow():
     assert len(hash_map) == len(TEST_KEYS_FOR_OVERFLOW)
 
 
-def test_integer_key():
-    hash_map = HashMap()
-    hash_map[1] = 2
-    assert hash_map[1] == 2
-
-
-def test_none_key():
-    hash_map = HashMap()
-    hash_map[None] = 2
-    assert hash_map[None] == 2
-
-
 def test_item_is_tuple():
     hash_map = HashMap()
     hash_map["foo"] = "bar"
     item = hash_map.storage[hash_map._get_hash("foo")]
     assert isinstance(item, tuple)
-
-def test_list_as_key():
-    hash_map = HashMap()
-    with pytest.raises(KeyError):
-        hash_map[[]] = 2
-
-def test_dict_as_key():
-    hash_map = HashMap()
-    with pytest.raises(KeyError):
-        hash_map[{"a": 1}] = 2
